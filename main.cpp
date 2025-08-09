4f83bea2fc7592776a5ac19e5f73ffa5d28d1b20
Alex12-Git200 <alexgkorntoulava@gmail.com>
2025-08-09 23:56:59 +0200

    Added smooth movement

--- main.cpp
+++ main.cpp
@@ -1,29 +1,47 @@
 #include "raylib.h"
 #include <iostream>
 
+// VARIABLES
+
+const int screenWidth = 800;
+const int screenHeight = 450;
+
+// player variables
+int playerX = 400;
+int playerY = 250;
+float playerR = 20.0;
+Color playerColor = BLACK;
+Color playerColors[] = {BLACK, BLUE, RED, PINK};
+int playerColorIndex = 0;
+bool touchingGround = true;
+int playerTerminalSpeed = 20;
+float playerVelocityY = 0;
+float playerVelocityX = 0;
+float playerDampening  = 0.9f;
+int playerJumpHeight = 10;
+float yVelPerFrame = 0.7;
+
+
+// ground variables
+int groundX = 0;
+int groundY = 270;
+int groundW = 800;
+int groundH = 200;
+Color groundColor = GREEN;
+
+// word vars
+const float gravity = 6;
+float fricion = 0.97;
+
+
+bool isPositive(float num) 
+{
+    return num > 0;
+}
+
 
 int main(void)
 {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
-    // player variables
-    int playerX = 400;
-    int playerY = 250;
-    float playerR = 20.0;
-    Color playerColor = BLACK;
-    Color playerColors[] = {BLACK, BLUE, RED, PINK};
-    int playerColorIndex = 0;
-    bool touchingGround = true;
-    int playerTerminalSpeed = 10;
-    float playerVelocity = 0;
-
-    // ground variables
-    int groundX = 0;
-    int groundY = 270;
-    int groundW = 800;
-    int groundH = 100;
-    Color groundColor = GREEN;
 
     InitWindow(screenWidth, screenHeight, "Ball Game");
 
@@ -42,14 +60,42 @@ int main(void)
             DrawCircle(playerX, playerY, playerR, playerColors[playerColorIndex]);  // draw player
             DrawRectangle(groundX, groundY, groundW, groundH, groundColor);
 
+            // Update player's velocity to gravity
+            playerVelocityY += yVelPerFrame;
+            if (playerVelocityY > gravity)
+            {
+                playerVelocityY = gravity;  // Cap the velocity to gravity
+            }
+            // Update player's Y position based on velocity
+            playerY += playerVelocityY;
+
+            // Update player's X position based on velocity
+            playerX += playerVelocityX;
+
+            if (playerVelocityX > playerTerminalSpeed - 0.5f)
+            {
+                playerVelocityX = playerTerminalSpeed;  // Cap the velocity to terminal speed
+            }
+            if ( playerVelocityX < -playerTerminalSpeed - 0.5f)
+            {
+                playerVelocityX = -playerTerminalSpeed;  // Cap the velocity to terminal speed
+            }
+
+            // checking if player is touching the ground
             if (playerY + playerR >= groundY)
             {
-                touchingGround = true;
-                std::cout << "\nTouching ground is True\n";
+                touchingGround = true; 
+                playerY = groundY - playerR;  // Reset player's position to be on top of the ground
+                playerVelocityY = 0;  // Reset vertical velocity when touching the ground
             }
             else
             {
-                std::cout << "\nTouching ground is falsen\n";
+                touchingGround = false;
+            }
+
+            if (!touchingGround)
+            {
+                yVelPerFrame = 0.4;
             }
 
 
@@ -63,13 +109,43 @@ int main(void)
             }
             if (IsKeyDown(KEY_RIGHT))
             {
-                playerX += playerTerminalSpeed;
+                playerVelocityX += 0.5f;  
+            }
+            if (!IsKeyDown(KEY_RIGHT))
+            {
+                if (playerVelocityX < 0.5 && isPositive(playerVelocityX) && touchingGround)
+                {
+                    playerVelocityX = 0;
+                }
+                if (playerVelocityX > 0 && touchingGround)
+                {
+                    playerVelocityX *= fricion;
+                }
             }
             if (IsKeyDown(KEY_LEFT))
             {
-                playerX -= playerTerminalSpeed;
+                playerVelocityX -= 0.5f;  
+            }
+            if (!IsKeyDown(KEY_LEFT))
+            {
+                if (playerVelocityX > -0.5 && !isPositive(playerVelocityX))
+                {
+                    playerVelocityX = 0;
+                }
+                if (playerVelocityX < 0 && touchingGround)
+                {
+                    playerVelocityX *= fricion;
+                }  
+            }
+            if (IsKeyPressed(KEY_SPACE) && touchingGround)
+            {
+                playerVelocityY = -playerJumpHeight;  // Set the player's velocity to jump height
+                touchingGround = false;  
             }
 
+            // debug
+
+            std::cout << "Y: " << playerY << " | X: " << playerX << " | VY: " << playerVelocityY << " | VX: " << playerVelocityX << "| TG: " << touchingGround << "\n";
 
 
         EndDrawing();
